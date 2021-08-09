12cca8d585f7aee7681e45106f37626df84e6459
takano630 <t.takano@4nonome.com>
2021-08-09 10:41:20 +0900

    secretkey change

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cq4jse8kd2i+hd^#4&g#5u0juwob-if6f)(8407qotp2hv%r$f'
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
